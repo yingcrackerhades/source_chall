08ec7bbbed29ed7d4cd453449c415649844f505c
Hades <101575881+yingcrackerhades@users.noreply.github.com>
2023-01-19 01:21:12 +0700

    Update conversation.py

--- misc/Percakapan/conversation.py
+++ misc/Percakapan/conversation.py
@@ -1,1 +1,13 @@
 
+#!/usr/bin/env python3
+from time import sleep
+
+print('Hello ctfer')
+sleep(1)
+print('You know my favorite number?')
+sleep(1)
+number = input('> ')
+print('%s not my favorite number' %number)
+sleep(1)
+print('If you can guess mu favorite number, i will give you a gift')
+exit(1)
